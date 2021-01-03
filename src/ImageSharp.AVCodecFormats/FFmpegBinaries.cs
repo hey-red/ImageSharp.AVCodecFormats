8ef49c50c979269d567bdcab3ea9619536dd500b
hey-red <hey.red@outlook.com>
2021-01-03 18:35:48 +0300

    Add proxy to ffmpeg binaries helper

---
+++ src/ImageSharp.AVCodecFormats/FFmpegBinaries.cs
@@ -0,0 +1,16 @@
+﻿using FFMediaToolkit;
+
+namespace HeyRed.ImageSharp.AVCodecFormats
+{
+    public static class FFmpegBinaries
+    {
+        /// <summary>
+        /// Get or sets path to directory with FFMpeg binaries.
+        /// </summary>
+        public static string? Path
+        {
+            get => FFmpegLoader.FFmpegPath;
+            set => FFmpegLoader.FFmpegPath = value;
+        }
+    }
+}
