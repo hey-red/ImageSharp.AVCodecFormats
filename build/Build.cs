fadcbedf14ae0592d0d1e154098b6a7de9d9a751
red <vforviolence@outlook.com>
2022-03-20 18:44:12 +0300

    Update native packages

--- build/Build.cs
+++ build/Build.cs
@@ -14,7 +14,7 @@ namespace Build
 {
     public class Build : NukeBuild
     {
-        private const string NATIVE_VERSION = "1.5.0";
+        private const string NATIVE_VERSION = "1.6.0";
 
         private readonly IEnumerable<string> _architectures = new[] { "linux-x64", "win-x64" };
 
@@ -23,62 +23,62 @@ namespace Build
         private readonly string _binPath = Path.Combine(RootDirectory, "build", "binaries");
 
         private Target DownloadBinaries => _ => _
-                .Executes(async () =>
-                {
-                    var httpClient = new HttpClient();
+                 .Executes(async () =>
+                 {
+                     var httpClient = new HttpClient();
 
-                    foreach (var architecture in _architectures)
-                    {
-                        string libTmpPath = Path.Combine(_binPath, architecture);
-                        string tmpFile = Path.GetTempFileName();
+                     foreach (var architecture in _architectures)
+                     {
+                         string libTmpPath = Path.Combine(_binPath, architecture);
+                         string tmpFile = Path.GetTempFileName();
 
-                        var uri = $"https://github.com/hey-red/FFmpeg-Build/releases/download/{NATIVE_VERSION}/{architecture}-noencoders.tar.gz";
+                         var uri = $"https://github.com/hey-red/FFmpeg-Build/releases/download/{NATIVE_VERSION}/{architecture}-noencoders.tar.gz";
 
-                        Serilog.Log.Information($"Download tarball from {uri}");
-                        using var response = await httpClient.GetStreamAsync(uri);
+                         Serilog.Log.Information($"Download tarball from {uri}");
+                         using var response = await httpClient.GetStreamAsync(uri);
 
-                        using (var fs = File.OpenWrite(tmpFile))
-                        {
-                            await response.CopyToAsync(fs);
-                        }
+                         using (var fs = File.OpenWrite(tmpFile))
+                         {
+                             await response.CopyToAsync(fs);
+                         }
 
-                        Serilog.Log.Information($"Extract tarball to {libTmpPath}");
-                        BuildUtils.ExtractArchive(tmpFile, libTmpPath);
+                         Serilog.Log.Information($"Extract tarball to {libTmpPath}");
+                         BuildUtils.ExtractArchive(tmpFile, libTmpPath);
 
-                        File.Delete(tmpFile);
-                    }
-                });
+                         File.Delete(tmpFile);
+                     }
+                 });
 
         private Target CreateNuGetPackages => _ => _
-                .DependsOn(DownloadBinaries)
-                .Executes(() =>
-                {
-                    foreach (var architecture in _architectures)
-                    {
-                        NuGetPack(p => p
-                            .SetTargetPath(RootDirectory / "build/ImageSharp.AVCodecFormats.Native." + architecture + ".nuspec")
-                            .SetVersion(NATIVE_VERSION)
-                            .SetOutputDirectory(_packagesPath)
-                            .AddProperty("NoWarn", "NU5128"));
-                    }
+                 .DependsOn(DownloadBinaries)
+                 .Executes(() =>
+                 {
+                     foreach (var architecture in _architectures)
+                     {
+                         NuGetPack(p => p
+                             .SetTargetPath(RootDirectory / "build/ImageSharp.AVCodecFormats.Native." + architecture + ".nuspec")
+                             .SetVersion(NATIVE_VERSION)
+                             .SetOutputDirectory(_packagesPath)
+                             .AddProperty("NoWarn", "NU5128"));
+                     }
 
-                    NuGetPack(p => p
-                        .SetTargetPath(RootDirectory / "build/ImageSharp.AVCodecFormats.Native.nuspec")
-                        .SetVersion(NATIVE_VERSION)
-                        .SetOutputDirectory(_packagesPath)
-                        .AddProperty("NoWarn", "NU5128"));
-                });
+                     NuGetPack(p => p
+                         .SetTargetPath(RootDirectory / "build/ImageSharp.AVCodecFormats.Native.nuspec")
+                         .SetVersion(NATIVE_VERSION)
+                         .SetOutputDirectory(_packagesPath)
+                         .AddProperty("NoWarn", "NU5128"));
+                 });
 
         private Target CleanUp => _ => _
-                .Executes(() =>
-                {
-                    EnsureCleanDirectory(_packagesPath);
-                    EnsureCleanDirectory(_binPath);
-                });
+                 .Executes(() =>
+                 {
+                     EnsureCleanDirectory(_packagesPath);
+                     EnsureCleanDirectory(_binPath);
+                 });
 
         private Target All => _ => _
-                       .DependsOn(CleanUp)
-                       .DependsOn(CreateNuGetPackages);
+                        .DependsOn(CleanUp)
+                        .DependsOn(CreateNuGetPackages);
 
         public static int Main() => Execute<Build>(x => x.All);
     }
