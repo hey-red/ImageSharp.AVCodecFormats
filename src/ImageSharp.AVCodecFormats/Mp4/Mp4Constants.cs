fe750309f9b4c3311bfc2d2c76cd9a524b6d0943
red <vforviolence@outlook.com>
2023-03-17 14:25:23 +0300

    Move mp4 brands to constants

--- src/ImageSharp.AVCodecFormats/Mp4/Mp4Constants.cs
+++ src/ImageSharp.AVCodecFormats/Mp4/Mp4Constants.cs
@@ -8,11 +8,136 @@ internal static class Mp4Constants
 
     public static readonly IEnumerable<string> MimeTypes = new[] { "video/mp4", "application/mp4" };
 
+    /// <summary>
+    /// ISO Media
+    /// </summary>
     public static readonly byte[] FtypMarker =
     {
         0x66,   // f
         0x74,   // t
         0x79,   // y
         0x70,   // p
+    };
+
+    /// <summary>
+    /// MP4 Base Media v1 [IS0 14496-12:2003]
+    /// </summary>
+    public static readonly byte[] IsomBrandHeader =
+    {
+        0x69,   // i
+        0x73,   // s
+        0x6F,   // o
+        0x6D,   // m
+    };
+
+    /// <summary>
+    /// v2
+    /// </summary>
+    public static readonly byte[] Iso2BrandHeader =
+    {
+        0x69,   // i
+        0x73,   // s
+        0x6F,   // o
+        0x32,   // 2
+    };
+
+    /// <summary>
+    /// v4
+    /// </summary>
+    public static readonly byte[] Iso4BrandHeader =
+    {
+        0x69,   // i
+        0x73,   // s
+        0x6F,   // o
+        0x34,   // 4
+    };
+
+    /// <summary>
+    /// v5
+    /// </summary>
+    public static readonly byte[] Iso5BrandHeader =
+    {
+        0x69,   // i
+        0x73,   // s
+        0x6F,   // o
+        0x35,   // 5
+    };
+
+    /// <summary>
+    /// v6
+    /// </summary>
+    public static readonly byte[] Iso6BrandHeader =
+    {
+        0x69,   // i
+        0x73,   // s
+        0x6F,   // o
+        0x36,   // 6
+    };
+
+    /// <summary>
+    /// MP4 Base w/ AVC ext [ISO 14496-12:2005]
+    /// </summary>
+    public static readonly byte[] Avc1BrandHeader =
+    {
+        0x61,   // a
+        0x76,   // v
+        0x63,   // c
+        0x31,   // 1
+    };
+
+    /// <summary>
+    /// MPEG-21 [ISO/IEC 21000-9]
+    /// </summary>
+    public static readonly byte[] Mp21BrandHeader =
+    {
+        0x6D,   // m
+        0x70,   // p
+        0x32,   // 2
+        0x31,   // 1
+    };
+
+    /// <summary>
+    /// MP4 v1 [ISO 14496-1:ch13]
+    /// </summary>
+    public static readonly byte[] Mp41BrandHeader =
+    {
+        0x6D,   // m
+        0x70,   // p
+        0x34,   // 4
+        0x31,   // 1
+    };
+
+    /// <summary>
+    /// MP4 v2 [ISO 14496-14]
+    /// </summary>
+    public static readonly byte[] Mp42BrandHeader =
+    {
+        0x6D,   // m
+        0x70,   // p
+        0x34,   // 4
+        0x32,   // 2
+    };
+
+    /// <summary>
+    /// MPEG v4 system, 
+    /// Dynamic Adaptive Streaming over HTTP
+    /// </summary>
+    public static readonly byte[] DashBrandHeader =
+    {
+        0x64,   // d
+        0x61,   // a
+        0x73,   // s
+        0x68,   // h
+    };
+
+    /// <summary>
+    /// Apple iTunes Video (.M4V) Video
+    /// </summary>
+    public static readonly byte[] M4vBrandHeader =
+    {
+        0x4D,   // M
+        0x34,   // 4
+        0x56,   // V
+        0x20,
     };
 }
--- src/ImageSharp.AVCodecFormats/Mp4/Mp4FormatDetector.cs
+++ src/ImageSharp.AVCodecFormats/Mp4/Mp4FormatDetector.cs
@@ -21,88 +21,20 @@ public sealed class Mp4FormatDetector : IImageFormatDetector
         if (header.Length >= HeaderSize &&
             header[4..8].SequenceEqual(Mp4Constants.FtypMarker))
         {
-            // TODO: move to type markers
-            var subHeader = header[8..];
+            var subHeader = header[8..12];
+
             return
-                // MP4 Base Media v1 [IS0 14496-12:2003]
-                (
-                    subHeader[0] == 0x69 &&     // i
-                    subHeader[1] == 0x73 &&     // s
-                    subHeader[2] == 0x6F &&     // o
-                    subHeader[3] == 0x6D        // m
-                )
-                ||
-                // MP4 Base Media v2 [ISO 14496-12:2005]
-                (
-                    subHeader[0] == 0x69 &&     // i
-                    subHeader[1] == 0x73 &&     // s
-                    subHeader[2] == 0x6F &&     // o
-                    subHeader[3] == 0x32        // 2
-                )
-                ||
-                // 4
-                (
-                    subHeader[0] == 0x69 &&     // i
-                    subHeader[1] == 0x73 &&     // s
-                    subHeader[2] == 0x6F &&     // o
-                    subHeader[3] == 0x34        // 4
-                )
-                ||
-                // 5
-                (
-                    subHeader[0] == 0x69 &&     // i
-                    subHeader[1] == 0x73 &&     // s
-                    subHeader[2] == 0x6F &&     // o
-                    subHeader[3] == 0x35        // 5
-                )
-                ||
-                // 6
-                (
-                    subHeader[0] == 0x69 &&     // i
-                    subHeader[1] == 0x73 &&     // s
-                    subHeader[2] == 0x6F &&     // o
-                    subHeader[3] == 0x36        // 6
-                )
-                ||
-                // MP4 Base w/ AVC ext [ISO 14496-12:2005]
-                (
-                    subHeader[0] == 0x61 &&     // a
-                    subHeader[1] == 0x76 &&     // v
-                    subHeader[2] == 0x63 &&     // c
-                    subHeader[3] == 0x31        // 1
-                )
-                ||
-                // MP4 v1 [ISO 14496-1:ch13]
-                (
-                    subHeader[0] == 0x6D &&     // m
-                    subHeader[1] == 0x70 &&     // p
-                    subHeader[2] == 0x34 &&     // 4
-                    subHeader[3] == 0x31        // 1
-                )
-                ||
-                // MP4 v2 [ISO 14496-14]
-                (
-                    subHeader[0] == 0x6D &&     // m
-                    subHeader[1] == 0x70 &&     // p
-                    subHeader[2] == 0x34 &&     // 4
-                    subHeader[3] == 0x32        // 2
-                )
-                ||
-                // MPEG v4 system
-                // Dynamic Adaptive Streaming over HTTP
-                (
-                    subHeader[0] == 0x64 &&     // d
-                    subHeader[1] == 0x61 &&     // a
-                    subHeader[2] == 0x73 &&     // s
-                    subHeader[3] == 0x68        // h
-                )
-                ||
-                // Apple M4V
-                (
-                    subHeader[0] == 0x4D &&     // M
-                    subHeader[1] == 0x34 &&     // 4
-                    subHeader[2] == 0x56        // V
-                );
+                subHeader.SequenceEqual(Mp4Constants.IsomBrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Iso2BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Iso4BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Iso5BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Iso6BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Avc1BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Mp21BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Mp41BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.Mp42BrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.DashBrandHeader) ||
+                subHeader.SequenceEqual(Mp4Constants.M4vBrandHeader);
         }
 
         return false;
