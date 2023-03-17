02dfaec7688499d01881ed8cc143fbaa19abc44d
red <vforviolence@outlook.com>
2023-03-17 12:52:51 +0300

    Move ASF header to wmv constants

--- src/ImageSharp.AVCodecFormats/Wmv/WmvConstants.cs
+++ src/ImageSharp.AVCodecFormats/Wmv/WmvConstants.cs
@@ -7,4 +7,24 @@ internal static class WmvConstants
     public static readonly IEnumerable<string> FileExtensions = new[] { "wmv" };
 
     public static readonly IEnumerable<string> MimeTypes = new[] { "video/x-ms-wmv", };
+
+    public static readonly byte[] AsfFormatHeader =
+    {
+        0x30,
+        0x26,
+        0xB2,
+        0x75,
+        0x8E,
+        0x66,
+        0xCF,
+        0x11,
+        0xA6,
+        0xD9,
+        0x00,
+        0xAA,
+        0x00,
+        0x62,
+        0xCE,
+        0x6C
+    };
 }
--- src/ImageSharp.AVCodecFormats/Wmv/WmvFormatDetector.cs
+++ src/ImageSharp.AVCodecFormats/Wmv/WmvFormatDetector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 using SixLabors.ImageSharp.Formats;
 
@@ -20,23 +21,7 @@ public sealed class WmvFormatDetector : IImageFormatDetector
     {
         if (header.Length >= HeaderSize)
         {
-            return
-                header[0] == 0x30 &&
-                header[1] == 0x26 &&
-                header[2] == 0xB2 &&
-                header[3] == 0x75 &&
-                header[4] == 0x8E &&
-                header[5] == 0x66 &&
-                header[6] == 0xCF &&
-                header[7] == 0x11 &&
-                header[8] == 0xA6 &&
-                header[9] == 0xD9 &&
-                header[10] == 0x00 &&
-                header[11] == 0xAA &&
-                header[12] == 0x00 &&
-                header[13] == 0x62 &&
-                header[14] == 0xCE &&
-                header[15] == 0x6C;
+            return header[..16].SequenceEqual(WmvConstants.AsfFormatHeader);
         }
 
         return false;
